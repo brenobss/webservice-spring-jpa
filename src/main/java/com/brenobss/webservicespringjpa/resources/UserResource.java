c7135376780737d13d9b92116db1c0c2e72b2995
Breno Barreto <brenobarreto5@hotmail.com>
2023-01-20 12:43:43 -0300

    create h2

--- src/main/java/com/brenobss/webservicespringjpa/resources/UserResource.java
+++ src/main/java/com/brenobss/webservicespringjpa/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.brenobss.webservicespringjpa.resources;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,10 @@ import com.brenobss.webservicespringjpa.entities.User;
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@GetMapping
 	public ResponseEntity<User> findAll(){
-		return null;
+		User user = new User(1L, "Luis", "luis@email.com", "99999999", "1234");
+		return ResponseEntity.ok().body(user);
 	}
 	
 }
