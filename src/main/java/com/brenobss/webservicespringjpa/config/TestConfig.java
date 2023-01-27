73ee2fd3a30d3d5e95348465611355c8aefc9de6
Breno Barreto <brenobarreto5@hotmail.com>
2023-01-27 10:47:37 -0300

    create enum OrderStatus

--- src/main/java/com/brenobss/webservicespringjpa/config/TestConfig.java
+++ src/main/java/com/brenobss/webservicespringjpa/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.brenobss.webservicespringjpa.entities.Order;
 import com.brenobss.webservicespringjpa.entities.User;
+import com.brenobss.webservicespringjpa.entities.enums.OrderStatus;
 import com.brenobss.webservicespringjpa.repositories.OrderRepository;
 import com.brenobss.webservicespringjpa.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner{
 		User maria = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		User alex = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), maria); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), alex); 
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), maria);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, maria); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, alex); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, maria);
 		
 		userRepository.saveAll(Arrays.asList(maria, alex));
 		
